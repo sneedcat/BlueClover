a618c46208d969fe0a3865460dfa40b0cb2899b8
meguminloli <meguminloli@protonmail.com>
2022-04-02 14:17:50 +0300

    added theme from chandevel#778

--- app/src/main/java/org/floens/chan/ui/theme/ThemeHelper.java
+++ app/src/main/java/org/floens/chan/ui/theme/ThemeHelper.java
@@ -62,6 +62,7 @@ public class ThemeHelper {
         themes.add(new DarkTheme("Gruvbox", "gruvbox", R.style.Chan_Theme_Gruvbox, PrimaryColor.DARK));
         themes.add(new DarkTheme("Neon", "neon", R.style.Chan_Theme_Neon, PrimaryColor.DARK));
         themes.add(new DarkTheme("Solarized Dark", "solarized_dark", R.style.Chan_Theme_SolarizedDark, PrimaryColor.ORANGE));
+        themes.add(new DarkTheme("Gee", "gee", R.style.Chan_Theme_Gee, PrimaryColor.DARK));
 
         ChanSettings.ThemeColor settingTheme = ChanSettings.getThemeAndColor();
         for (Theme theme : themes) {
